c433524fdb3423ce4fbafb2f9d2dbb148be8ffad
Ilya Petrovanov <ispetrovanov@edu.hse.ru>
2024-11-05 10:14:42 +0300

    delete model for multiple antennes

--- awgn_channel_dmrs.m
+++
@@ -1,7 +0,0 @@
-function rx=awgn_channel_dmrs(tx_ifft_seq,nFFT,CP_len,sym_1,sym_2,noise_var,delay)
-    %%%%%AWGN channel%%%%%%
-    noise = (randn(size(tx_ifft_seq)) + 1i * randn(size(tx_ifft_seq))) / sqrt(2);
-    noise((nFFT + CP_len) * (sym_1 - 1) + 1 + delay:(nFFT + CP_len) * sym_1 + delay) = complex(zeros(1, nFFT + CP_len));
-    noise((nFFT + CP_len) * (sym_2 - 1) + 1 + delay:(nFFT + CP_len) * sym_2 + delay) = complex(zeros(1, nFFT + CP_len));
-    rx = tx_ifft_seq + sqrt(noise_var) * noise;
-end
--- calc_evm_zf.m
+++
@@ -1,9 +0,0 @@
-function evm_zf = calc_evm_zf(S, nOFDM, n, tx_qam, rx_fft_zf)
-    evm_zf=zeros(S,nOFDM-2);
-
-    for ss=1:S
-        for jj=1:nOFDM-2
-           evm_zf(ss,jj)=calculate_evm(tx_qam(ss,:,jj).',rx_fft_zf(:,jj,ss),n);
-        end
-    end
-end
